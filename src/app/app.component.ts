6a9e3ff1c6fd4ded28ed588a50daeae0dadd5bda
Samila Gunarathna <65965346+SAMI-GD@users.noreply.github.com>
2023-02-10 03:30:02 +0530

    comments added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,39 +4,48 @@ import { ApiService } from './service/api.service';
 import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MaterialDialogComponent } from './dialog/material-dialog.component';
 
-import {MatPaginator} from '@angular/material/paginator';
-import {MatSort} from '@angular/material/sort';
-import {MatTableDataSource} from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import { inject } from '@angular/core/testing';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+// Implementing OnInit interface to run a code on component initialization
 export class AppComponent implements OnInit {
+  // Defining the columns to be displayed in the table
   title = 'pro-payroll-front-end'; displayedColumns: string[] = ['id', 'name', 'legalName', 'abn', 'acn', 'active', 'actions'];
   dataSource!: MatTableDataSource<any>;
 
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
+
+  // Injecting the MatDialog and ApiService
   constructor(private dialog: MatDialog, private apiService: ApiService,
-    ) { }
+  ) { }
   ngOnInit(): void {
     this.getBusinesses();
   }
-
+  // Method to fetch businesses data from the API
   getBusinesses() {
     this.apiService.getBusinesses().subscribe(res => {
       this.dataSource = new MatTableDataSource(res);
       this.dataSource.paginator = this.paginator;
       this.dataSource.sort = this.sort;
-      console.log(res); 
+      console.log(res);
     }, err => { console.log(err.error.message); });      // this.dataSource.filter = this.filter;
-    
+
   }
-  editBusiness(row : any) {
+
+  // Method to open the dialog for editing a business
+  editBusiness(row: any) {
 
     this.dialog.open(MaterialDialogComponent, {
       width: '30%',
@@ -47,7 +56,7 @@ export class AppComponent implements OnInit {
   }
 
 
-
+// Method to delete a business by its id
   deleteBusiness(id: number) {
     this.apiService.deleteBusiness(id).subscribe(res => {
       alert("Business deleted successfully");
@@ -55,15 +64,19 @@ export class AppComponent implements OnInit {
     }, err => { alert(err.error.message); });
   }
 
+
+  // Method to open the dialog for adding a business
   openDialog() {
-    this.dialog.open(MaterialDialogComponent,{
+    this.dialog.open(MaterialDialogComponent, {
       width: '30%',
     }).afterClosed().subscribe(res => {
       if (res == "save") { this.getBusinesses(); }
     });
   }
 
-applyFilter(event: Event) {
+
+  // Method to filter the table data
+  applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
     if (this.dataSource.paginator) {
--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -8,22 +8,30 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class ApiService {
 
+  // Define API URL
   private apiServerUrl = 'http://localhost:8080/business';
 
+
+  // Inject HttpClient into your component or service.
   constructor(private http: HttpClient) { }
 
+  // Define the method to fetch the businesses data
   public getBusinesses(): Observable<Business[]> {
     return this.http.get<Business[]>(`${this.apiServerUrl}/all`);
   }
+
+  // Define the method to post the businesses data
   public addBusiness(business: Business): Observable<Business> {
     return this.http.post<Business>(`${this.apiServerUrl}/add`, business);
   }
+
+  // Define the method to update the businesses dat
   public updateBusiness(data:any,id: number){
     return this.http.put(`${this.apiServerUrl}/update/${id}`,data);
   }
 
 
-
+// Define the method to delete the businesses data 
   public deleteBusiness(businessId: number): Observable<void> {
     return this.http.delete<void>(`${this.apiServerUrl}/delete/${businessId}`);
   }
